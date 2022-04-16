06ba6199a5d925359c0c1d9f509beb6e48cc68ef
AfifaAli27 <103840483+AfifaAli27@users.noreply.github.com>
2022-04-16 22:26:53 +0500

    Create addition.cpp

---
+++ addition.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int num1, num2, sum;
+	cout << "Enter two integers:";
+	cin>>num1>>num2;
+	sum= num1+num2;
+	
+	cout << num1 << "+" << num2 << "=" << sum;
+	
+	return 0;
+	
+}
