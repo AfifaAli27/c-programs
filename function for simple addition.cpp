4bc25b5763d46642888d9e73b3c725329706b9d6
AfifaAli27 <103840483+AfifaAli27@users.noreply.github.com>
2022-05-14 17:16:42 +0500

    Create function for simple addition.cpp

---
+++ function for simple addition.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int valueOne = 5;
+int valueTwo = 6;
+void Add();
+int main()
+{	
+	Add();
+}
+
+void Add()
+{
+
+	cout<<"The sum of two integers is --> "<<valueOne + valueTwo<<endl;
+}
